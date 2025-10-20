f311bc6a979530bae08424177e6f4875f7a2846f
Ferdinand Thiessen <opensource@fthiessen.de>
2025-10-20 15:30:22 +0200

    chore: setup linting
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,61 +1,27 @@
-// See: https://eslint.org/docs/latest/use/configure/configuration-files
+/*!
+ * SPDX-FileCopyrightText: 2025 Ferdinand Thiessen
+ * SPDX-License-Identifier: MIT
+ */
 
-import { fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import _import from 'eslint-plugin-import'
-import jest from 'eslint-plugin-jest'
-import prettier from 'eslint-plugin-prettier'
+import { recommendedLibrary } from '@nextcloud/eslint-config'
 import globals from 'globals'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
-})
 
 export default [
-  {
-    ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules']
-  },
-  ...compat.extends(
-    'eslint:recommended',
-    'plugin:jest/recommended',
-    'plugin:prettier/recommended'
-  ),
-  {
-    plugins: {
-      import: fixupPluginRules(_import),
-      jest,
-      prettier
-    },
+	{
+		ignores: ['**/coverage', '**/dist', '**/linter', '**/node_modules'],
+	},
+	...recommendedLibrary,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+				Atomics: 'readonly',
+				SharedArrayBuffer: 'readonly',
+			},
 
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
-      },
-
-      ecmaVersion: 2023,
-      sourceType: 'module'
-    },
-
-    rules: {
-      camelcase: 'off',
-      'eslint-comments/no-use': 'off',
-      'eslint-comments/no-unused-disable': 'off',
-      'i18n-text/no-en': 'off',
-      'import/no-namespace': 'off',
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-unused-vars': 'off',
-      'prettier/prettier': 'error'
-    }
-  }
+			ecmaVersion: 2023,
+			sourceType: 'module',
+		},
+	},
 ]
